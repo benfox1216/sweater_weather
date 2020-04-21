2a9c9768542d3d3f69d0e7bd5294ec26c3719746
Ben Fox <benfox1216@gmail.com>
2020-04-21 07:51:27 -0600

    Set up file structure for forecast & backgrounds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'backgrounds/index'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'forecast/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :forecast, only: [:show]
+      resources :backgrounds, only: [:show]
+    end
+  end
 end
---
+++ db/migrate/20200421131117_create_backgrounds.rb
@@ -0,0 +1,8 @@
+class CreateBackgrounds < ActiveRecord::Migration[5.1]
+  def change
+    create_table :backgrounds do |t|
+
+      t.timestamps
+    end
+  end
+end
